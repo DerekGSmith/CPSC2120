df80f84312b4d7e89ed4f425ad77dfcc3f076995
DerekGSmith <dgs4@clemson.edu>
2023-09-25 22:57:36 -0400

    implementation completed

--- stack-postfix-calculator/T1.cpp
+++ stack-postfix-calculator/T1.cpp
@@ -1,31 +1,9 @@
 #include <cstring>
+#include <iostream>
 #include <stack>
 #include <string>
 
 using namespace std;
-
-// Calculates a postfix expression with integer operands using a stack
-// The expression is represented using an array of strings
-// Each string is either an operand or operator symbol
-// Operator symbols:
-//"+" addition
-//"-" subtraction
-//"*" multiplication
-//"/" divison
-//"%" remainder
-// Example expression: "8", "5", "-" evaluates to a result of 3:
-// 1: push 8
-// 2: push 5,
-// 3: "-"-> pop top two values, earlier value is the left-side operand: 8 - 5
-// The result of the operation (8-5) is 3, push 3 onto stack
-// After evaluation of the expression, the final result should be
-// the only value on the stack, return 0 if the stack is
-// non-empty after popping the result
-// Return 0 if expression is zero-length or if there are insufficient operands
-// for an operation
-// The STL Stack class can be used
-// To easily convert a numeric string to an int you may use the stoi() function
-// which takes a string as a parameter and returns a string
 
 int isOperator(char o) {
     if ((o == '+') || (o == '-')) {
@@ -70,20 +48,26 @@ int calculatePostfixExpression(string expression[], int length) {
             switch (infixC[0]) {
                 case '+':
                     left = left + right;
+                    cout << left;
                     break;
                 case '-':
                     left = left - right;
+                    cout << left;
                     break;
                 case '*':
                     left = left * right;
+                    cout << left;
                     break;
                 case '/':
                     left = left / right;
+                    cout << left;
                     break;
                 case '%':
                     left = left % right;
+                    cout << left;
                     break;
             }
+            cout << left;
             postfixStack.push(left);
         }
     }
@@ -92,4 +76,16 @@ int calculatePostfixExpression(string expression[], int length) {
     } else {
         return postfixStack.top();
     }
+}
+int main() {
+    string exp[] = {"4", "5", "*"};
+
+    // ASSERT_EQ(calculatePostfixExpression(exp,3), 20);
+    if (calculatePostfixExpression(exp, 3) != 20) {
+        cout << "expected/correct value 20, actual value when testing "
+             << calculatePostfixExpression(exp, 3) << ".\n";
+        return 1;
+    }
+    cout << "Passed" << endl;
+    return 0;
 }
